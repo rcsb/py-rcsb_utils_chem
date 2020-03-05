114d316530ad217ba25aa3b7d5736d4f440ced67
jwest <john.westbrook@rcsb.org>
2020-03-05 11:36:41 -0500

    Handle platform dependent memory usage

--- rcsb/utils/tests-chem/testChemCompMoleculeProvider.py
+++ rcsb/utils/tests-chem/testChemCompMoleculeProvider.py
@@ -20,6 +20,7 @@ __license__ = "Apache 2.0"
 
 import logging
 import os
+import platform
 import resource
 import time
 import unittest
@@ -49,8 +50,9 @@ class ChemCompMoleculeProviderTests(unittest.TestCase):
         logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
 
     def tearDown(self):
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
         rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        logger.info("Maximum resident memory size %.4f MB", rusageMax / 10 ** 6)
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
         endTime = time.time()
         logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
 
--- rcsb/utils/tests-chem/testChemCompSearchIndexProvider.py
+++ rcsb/utils/tests-chem/testChemCompSearchIndexProvider.py
@@ -21,6 +21,7 @@ __license__ = "Apache 2.0"
 
 import logging
 import os
+import platform
 import resource
 import time
 import unittest
@@ -46,8 +47,9 @@ class ChemCompSearchIndexProviderTests(unittest.TestCase):
         logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
 
     def tearDown(self):
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
         rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        logger.info("Maximum resident memory size %.4f MB", rusageMax / 10 ** 6)
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
         endTime = time.time()
         logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
 
