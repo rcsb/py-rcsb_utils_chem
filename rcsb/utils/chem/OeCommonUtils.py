eaadf35a185a8d7bdd67de4e85c44b1baaa0c0ce
jwest <john.westbrook@rcsb.org>
2021-02-03 19:59:11 -0500

    V0.58 Add search criteria relaxed-stereo-sdeq and relaxed-sdeq

--- rcsb/utils/chem/OeCommonUtils.py
+++ rcsb/utils/chem/OeCommonUtils.py
@@ -27,17 +27,22 @@ class OeCommonUtils(object):
         elif matchOpts in ["relaxed-stereo", "graph-relaxed-stereo", "sub-struct-graph-relaxed-stereo"]:
             atomexpr = oechem.OEExprOpts_AtomicNumber | oechem.OEExprOpts_Chiral | oechem.OEExprOpts_FormalCharge
             bondexpr = oechem.OEExprOpts_BondOrder | oechem.OEExprOpts_Chiral
+        elif matchOpts in ["relaxed-stereo-sdeq", "graph-relaxed-stereo-sdeq", "sub-struct-graph-relaxed-stereo-sdeq"]:
+            atomexpr = oechem.OEExprOpts_AtomicNumber | oechem.OEExprOpts_Chiral | oechem.OEExprOpts_FormalCharge | oechem.OEExprOpts_Aromaticity
+            bondexpr = oechem.OEExprOpts_BondOrder | oechem.OEExprOpts_Chiral | oechem.OEExprOpts_EqSingleDouble
         elif matchOpts in ["relaxed", "graph-relaxed", "simple", "sub-struct-graph-relaxed"]:
             atomexpr = oechem.OEExprOpts_AtomicNumber | oechem.OEExprOpts_FormalCharge
             bondexpr = oechem.OEExprOpts_BondOrder
+        elif matchOpts in ["graph-relaxed-sdeq", "sub-struct-graph-relaxed-sdeq"]:
+            atomexpr = oechem.OEExprOpts_AtomicNumber | oechem.OEExprOpts_FormalCharge | oechem.OEExprOpts_Aromaticity
+            bondexpr = oechem.OEExprOpts_BondOrder | oechem.OEExprOpts_EqSingleDouble
         elif matchOpts in ["exact"]:
             atomexpr = oechem.OEExprOpts_ExactAtoms
             bondexpr = oechem.OEExprOpts_ExactBonds
         else:
-            logger.error("Unanticipated match options %r", matchOpts)
             atomexpr = oechem.OEExprOpts_DefaultAtoms
             bondexpr = oechem.OEExprOpts_DefaultBonds
-            logger.error("Unanticipated match options %r", matchOpts)
+            logger.error("Unanticipated match options %r (applying defaults)", matchOpts)
         #
         #
         # oechem.OEExprOpts_EqSingleDouble
